8dc32a0b3fa6536f794e024fe31be3b6aae58a43
RomChaba <chabassier.r@gmail.com>
2018-02-14 14:22:04 +0100

    Entity

---
+++ AnimDispa/Controllers/ComptesController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using AnimDispa.Models;
+
+namespace AnimDispa.Controllers
+{
+    public class ComptesController : Controller
+    {
+        private AnimDispaContext db = new AnimDispaContext();
+
+        // GET: Comptes
+        public ActionResult Index()
+        {
+            return View(db.Comptes.ToList());
+        }
+
+        // GET: Comptes/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Comptes comptes = db.Comptes.Find(id);
+            if (comptes == null)
+            {
+                return HttpNotFound();
+            }
+            return View(comptes);
+        }
+
+        // GET: Comptes/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Comptes/Create
+        // Afin de déjouer les attaques par sur-validation, activez les propriétés spécifiques que vous voulez lier. Pour 
+        // plus de détails, voir  http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,Nom,Prenom,Login,Password,Mail,Telephone,Adresse,CodePostal,Ville")] Comptes comptes)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Comptes.Add(comptes);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(comptes);
+        }
+
+        // GET: Comptes/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Comptes comptes = db.Comptes.Find(id);
+            if (comptes == null)
+            {
+                return HttpNotFound();
+            }
+            return View(comptes);
+        }
+
+        // POST: Comptes/Edit/5
+        // Afin de déjouer les attaques par sur-validation, activez les propriétés spécifiques que vous voulez lier. Pour 
+        // plus de détails, voir  http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Nom,Prenom,Login,Password,Mail,Telephone,Adresse,CodePostal,Ville")] Comptes comptes)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(comptes).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(comptes);
+        }
+
+        // GET: Comptes/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Comptes comptes = db.Comptes.Find(id);
+            if (comptes == null)
+            {
+                return HttpNotFound();
+            }
+            return View(comptes);
+        }
+
+        // POST: Comptes/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Comptes comptes = db.Comptes.Find(id);
+            db.Comptes.Remove(comptes);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
