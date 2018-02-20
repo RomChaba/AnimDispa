909b1bbf7341833eed8e65f3cfae5ef48e25ece6
aclavel <antoine.clavel@sts-sio-caen.info>
2018-02-20 14:04:03 +0100

    module config

---
+++ AnimDispa/Controllers/AdminController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using AnimDispa.Models;
+using System.Web.Mvc;
+
+namespace AnimDispa.Controllers
+{
+    public class AdminController : Controller {
+
+        private AnimDispaContext db = new AnimDispaContext();
+        
+        public ActionResult Index() {
+            return View();
+        }
+    }
+}
--- AnimDispa/Models/Config.cs
+++ AnimDispa/Models/Config.cs
@@ -1,21 +1,28 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Web;
 
 namespace AnimDispa.Models {
 
+    [Table("Config")]
+
     public class Config {
 
         public int Id { get; set; }
         public string Libelle { get; set; }
-        public string Value { get; set; }
+        public bool Value { get; set; }
 
-        public Config(int id, string libelle, string value)
+        public Config(int id, string libelle, bool value)
         {
             Id = id;
             Libelle = libelle;
             Value = value;
+        }
+
+        public Config() {
+            
         }
     }
 }
